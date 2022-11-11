951d8c7d5a67e071e78600e3dce64aba9bf5f69f
Qiaoran Dong <dqiaoran@umich.edu>
2022-11-11 11:53:19 -0500

    build package(roxygen)

--- R/lireg.R
+++ R/lireg.R
@@ -12,6 +12,36 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#'slr
+#'
+#'Simple linear regression
+#'
+#'@param res_var
+#'
+#'@return fitted beta-zero and beta-one
+#'
+#'@examples
+#'slr(2,3)
+#'
+#'@export
+#'
 slr <- function(res_var, cov1) {
   print("Simple linear regression Y = X*beta_1 + beta_0")
+}
+
+#'mlr
+#'
+#'Multiple Linear Regression
+#'
+#'@param res_var
+#'
+#'@return fitted beta-zero and beta-one
+#'
+#'@examples
+#'slr(2,3)
+#'
+#'@export
+#'
+mlr <- function(res_var, cov){
+  print("Multiple linear regression")
 }
