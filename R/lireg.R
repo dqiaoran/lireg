e1001127fe022417a7afcd1d18d0ffcb2d93d3f4
Qiaoran Dong <dqiaoran@umich.edu>
2022-11-11 10:42:03 -0500

    newly created package

---
+++ R/lireg.R
@@ -0,0 +1,17 @@
+# Linear Regression
+#
+# The package is designed to do a simple linear regression
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+slr <- function(res_var, cov1) {
+  print("Simple linear regression Y = X*beta_1 + beta_0")
+}
